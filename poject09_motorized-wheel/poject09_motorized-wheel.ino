99bbb9b64e9ac45d23cfa9bd42aea6d0efd66402
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2020-12-20 13:03:30 -0500

    Finished project09.

--- poject09_motorized-wheel/poject09_motorized-wheel.ino
+++ poject09_motorized-wheel/poject09_motorized-wheel.ino
@@ -1,9 +1,23 @@
-void setup() {
-  // put your setup code here, to run once:
+// Name your constants and variables
+const int switchPin = 2;
+const int motorPin = 9;
+int switchState = 0;
 
+void setup() {
+  // Declasre the pins' direction
+  pinMode(motorPin, OUTPUT);
+  pinMode(switchPin, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // Read the input
+  switchState = digitalRead(switchPin);
+  
+  // Pull the output high if pressed
+  if (switchState == HIGH) {
+    digitalWrite(motorPin, HIGH);
+  }
+  else {
+    digitalWrite(motorPin, LOW);
+  }
 }
