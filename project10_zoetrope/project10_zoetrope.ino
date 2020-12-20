32bfc18f24815ae0563feb8820c93bb03f4ed3a8
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2020-12-20 16:04:03 -0500

    Finish project10.

--- project10_zoetrope/project10_zoetrope.ino
+++ project10_zoetrope/project10_zoetrope.ino
@@ -1,9 +1,76 @@
-void setup() {
-  // put your setup code here, to run once:
+// Name your constants
+const int controlPin1 = 2;
+const int controlPin2 = 3;
+const int enablePin = 9;
+const int directionSwitchPin = 4;
+const int onOffSwitchStateSwitchPin = 5;
+const int potPin = A0;
 
+// Create variables for remembering program state
+int onOffSwitchState = 0;
+int previousOnOffSwitchState = 0;
+int directionSwitchState = 0;
+int previousDirectionSwitchState = 0;
+
+// Create variables for motor control
+int motorEnabled = 0;
+int motorSpeed = 0;
+int motorDirection = 1;
+
+void setup() {
+  // Declare the digital pins as inputs and outputs
+  pinMode(directionSwitchPin, INPUT);
+  pinMode(onOffSwitchStateSwitchPin, INPUT);
+  pinMode(controlPin1, OUTPUT);
+  pinMode(controlPin2, OUTPUT);
+  pinMode(enablePin, OUTPUT);
+
+  // Turn the motor off
+  digitalWrite(enablePin, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Read sensor information
+  onOffSwitchState = digitalRead(onOffSwitchStateSwitchPin);
+  delay(1);
+  directionSwitchState = digitalRead(directionSwitchPin);
+  motorSpeed = analogRead(potPin)/4;
 
+
+  // Check if on/off sensor has changed
+  if(onOffSwitchState != previousOnOffSwitchState){
+    if(onOffSwitchState == HIGH){
+      motorEnabled = !motorEnabled;
+    }
+  }
+
+  // Check to see of the direction has changed
+  if (directionSwitchState != previousDirectionSwitchState) {
+    if (directionSwitchState == HIGH) {
+      motorDirection = !motorEnabled;
+    }
+  }
+
+  // Change the pins to turn the motor in the proper direction
+  if (motorDirection == 1) {
+    digitalWrite(controlPin1, HIGH);
+    digitalWrite(controlPin2, LOW);
+  }
+
+  else {
+    digitalWrite(controlPin1, LOW);
+    digitalWrite(controlPin2, HIGH);
+  }
+
+  // PMW te motor if it is enabled
+  if (motorEnabled == 1) {
+    analogWrite(enablePin, motorSpeed);
+  }
+  else {
+    analogWrite(enablePin, 0);
+  }
+
+  // Save the current states for the next loop
+  previousDirectionSwitchState = directionSwitchState;
+  previousOnOffSwitchState = onOffSwitchState;
 }
